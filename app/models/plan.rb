f651358633a2b7f0aa5ac89d5d176359f5ef60f9
o281700 <siba0311@gmail.com>
2020-12-17 22:04:24 +0900

    user link設定

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,5 @@
 class Plan < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :plans, dependent: :destroy
+
+  validates :nickname, presence: true
 end
