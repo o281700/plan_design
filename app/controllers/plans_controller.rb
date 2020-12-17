0958b68c72e9a39ed0ce5f9556b503b8977978a1
o281700 <siba0311@gmail.com>
2020-12-18 01:32:21 +0900

    アクション設定

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,24 @@
 class PlansController < ApplicationController
+  def index
+    @plans = Plan.includes(:user)
+  end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:title).merge(user_id: crrent_user.id)
+  end
 end
